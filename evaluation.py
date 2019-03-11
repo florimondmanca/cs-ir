8b8c3dc393dadcadcb861e2a7159ccecf93eebef
aldebout <alexdeboutray@gmail.com>
2019-03-11 11:38:02 +0100

    Fixed most of performance eval

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,8 @@
 from time import time
 from indexes import build_index
 import click
-from models.boolean import Q
-from models.vector import vector_search
+from models.boolean.search import Q
+from models.vector.search import vector_search
 import os
 
 def evaluate_performance(collection):
@@ -22,17 +22,18 @@ def evaluate_performance(collection):
     click.echo(results)
     click.echo("Results found in {time:.6f}s".format(time=(t1-t0)))
 
-    """t0 = time()
+    t0 = time()
     query = "algorithm artificial"
     click.echo(f"Executing query \"{query}\"...")
     results = vector_search(query, index)
     t1 = time()
 
     click.echo(results)
-    click.echo("Results found in {time:.6f}s".format(time=(t1-t0)))"""
+    click.echo("Results found in {time:.6f}s".format(time=(t1-t0)))
 
     # Disk space
     for file in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache")):
-        click.echo("{file} ---- {file_size}MB".format(file=file, file_size=(os.stat(os.path.abspath(file)).st_size >> 20)))
+        click.echo("{file} ---- {file_size:.3f}MB".format(file=file, file_size=(os.stat(os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache", file)).st_size >> 20)))
+        click.echo(os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache", file))
     
 
