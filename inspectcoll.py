6596006747996193156dba7e3192d76b3268445d
florimondmanca <florimond.manca@gmail.com>
2019-03-12 21:22:18 +0100

    fix: invoke showperfs in inspectcoll

--- inspectcoll.py
+++ inspectcoll.py
@@ -8,14 +8,15 @@ from cli_utils import CollectionType
 from data_collections import Collection
 from heaps import estimate, create_heaps
 
-from evaluation import evaluate_performance, get_requests
+from evaluation.cli import cli as eval_cli
 
 load_dotenv()
 
 
 @click.command()
 @click.argument("collection", type=CollectionType())
-def cli(collection: Collection):
+@click.pass_context
+def cli(ctx: click.Context, collection: Collection):
     """Inspect a collection and display key metrics.
 
     - Number of documents
@@ -85,7 +86,7 @@ def cli(collection: Collection):
     plt.show()
 
     # Evaluation
-    evaluate_performance(collection)
+    ctx.invoke(eval_cli.get_command("showperfs"), collection=collection)
 
 
 if __name__ == "__main__":
